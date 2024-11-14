1da8277eaffb3093e8c7a031eafa59f1fd2e9db0
brysongroves <grovesbryson54@gmail.com>
2024-11-14 13:10:20 -0500

    wip

--- index5.php
+++ index5.php
@@ -4,6 +4,7 @@ session_start();
 require_once 'auth.php';
 
 // Check if user is logged in
+// ANY STUPID COMMENT
 if (!is_logged_in()) {
     header('Location: login.php');
     exit;
@@ -11,8 +12,8 @@ if (!is_logged_in()) {
 
 $host = 'localhost'; 
 $dbname = 'books'; 
-$user = 'mark'; 
-$pass = 'mark';
+$user = 'bryson'; 
+$pass = 'bryson';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
